d26b040f91cbeec93bd6f9eb647867f9b82d86a7
masaki925 <masaki925@gmail.com>
2013-07-13 18:04:57 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Compathy::Application.config.secret_key_base = '0ff9797dbe4fcac1ef1432d9cabbfce44cb8e57f331a0bf957fcc6791453cd9a03bd34e07f68f9ad5c5db9983a88991e5752637893523d9e95584db5e6009c7b'
