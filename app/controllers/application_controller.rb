ea6c9e884ff6863f02640c2e98d030a2143a9559
masaki925 <masaki925@gmail.com>
2013-07-14 14:54:01 +0900

    認証をつける

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  if Rails.env.production?
+    http_basic_authenticate_with :name => ENV['BASIC_ID'], :password => ENV['BASIC_PASS']
+  end
+
   protected
   def not_authenticated
     redirect_to login_path, :alert => "Please login first."
