2b00d5a639955ff153064ff13a11b56fa5c86f4a
masaki925 <masaki925@gmail.com>
2013-07-17 09:06:00 +0900

    rake spec が通るように整理。oauths_controller のテストは苦しいが一旦テストが通るようにしておく

--- spec/controllers/oauths_controller_spec.rb
+++ spec/controllers/oauths_controller_spec.rb
@@ -1,18 +1,26 @@
 require 'spec_helper'
 
 describe OauthsController do
+  before do
+    @user = FactoryGirl.create(:user)
+
+    # login_from はsorcery のメソッド。この中でENV から拾ったcallback_url とかを使って、OAuth2 認証をする
+    # ただ、token を得るためにcallback 時の'code' を準備する必要があるが、すぐexpire したりして準備しづらいのと、
+    # ライブラリ自体のテストもあるので、ここではstub するだけにしておく
+    OauthsController.any_instance.stub(:login_from).and_return(@user)
+  end
 
   describe "GET 'oauth'" do
     it "returns http success" do
-      get 'oauth'
-      response.should be_success
+      get :oauth, { provider: "facebook" }, {}
+      response.status.should be(302)
     end
   end
 
   describe "GET 'callback'" do
     it "returns http success" do
-      get 'callback'
-      response.should be_success
+      get :callback, { provider: "facebook" }, {}
+      response.status.should redirect_to(root_path)
     end
   end
 
